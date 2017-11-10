9b10aa4b9adee2d077df940a311ec112394f13af
sasasiu <sasasiu@shenmegui.com>
2017-11-10 23:45:14 +0800

    xml

--- src/register/login.java
+++ src/register/login.java
@@ -19,13 +19,13 @@ public class login {
 		Statement stmt = null;
 		ResultSet rs1 = null,rs2 = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/sepractice?"
-					+ "useUnicode=true&characterEncoding=utf-8&useSSL=false";
-			String user = "root";
-			String psw = "5810267";
-			conn = DriverManager.getConnection(url,user,psw);
-			stmt = conn.createStatement();
+			Class.forName("com.mysql.jdbc.Driver"); 
+			String url = "jdbc:mysql://localhost:3306/sepractice?" 
+					+ "useUnicode=true&characterEncoding=utf-8&useSSL=false"; 
+			String user = "root"; 
+			String psw = "5810267"; 
+			conn = DriverManager.getConnection(url,user,psw); 
+			stmt = conn.createStatement(); 
 			String sql="select PassWord from teacher where UserName = \'"+username + "\'";
 			rs1 = stmt.executeQuery(sql);
 			if (rs1.next()) {
@@ -502,4 +502,6 @@ public class login {
 	public void setAllAch(String allAch) {
 		AllAch = allAch;
 	}
+	
+	
 }
